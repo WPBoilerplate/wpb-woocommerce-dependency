2106e017ac8563fd81650c6f7a95f7cdccd0fac5
raftaar1191 <raftaar1191@gmail.com>
2024-05-15 14:15:20 +0530

    Update class name

--- dependency.php
+++ dependency.php
@@ -5,8 +5,8 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Check if the class does not exits then only allow the file to add
  */
-if( ! class_exists( 'AcrossWP_BuddyBoss_Platform_Dependency' ) ) {
-    class AcrossWP_BuddyBoss_Platform_Dependency extends AcrossWP_Plugins_Dependency {
+if( ! class_exists( 'AcrossWP_WooCommerce_Dependency' ) ) {
+    class AcrossWP_WooCommerce_Dependency extends AcrossWP_Plugins_Dependency {
         /**
          * Load this function on plugin load hook
          * Example: _e('<strong>BuddyBoss Sorting Option In Network Search</strong></a> requires the BuddyBoss Platform plugin to work. Please <a href="https://buddyboss.com/platform/" target="_blank">install BuddyBoss Platform</a> first.', 'sorting-option-in-network-search-for-buddyboss');
